83fcec18bfaf688407ac342cfa00e0d5e432b2be
Gomes, J.M <02-armory-dancers@icloud.com>
2022-11-15 17:28:20 -0300

    scripts das vms com novo ip

--- xen/vms/labqs-c2.sh
+++ xen/vms/labqs-c2.sh
@@ -2,16 +2,16 @@
 set -euo pipefail
 
 sudo xen-create-image \
-	--hostname='c1.labqs.ita.br' \
+	--hostname='c2.labqs.ita.br' \
 	--memory=1gb \
 	--vcpus=2 \
 	--lvm=ubuntu-vg  \
     --size=20Gb \
-    --ip=172.31.100.1 \
+    --ip=172.31.100.2 \
     --netmask=172.31.0.0 \
     --broadcast=172.31.255.255 \
     --gateway=172.31.0.1 \
-    --nameserver=161.24.23.180 \
+    --nameserver=161.24.23.180 161.24.23.199 \
 	--randommac \
     --bridge=xenbr0 \
     --role=labqs-sshd \

1fb2696e7cd35bb74d0fb52173304f8721052386
José Marcos Gomes <jose.marcos.gomes@gmail.com>
2023-01-10 11:31:57 -0300

    ambiente de homologação preliminar

--- xen/vms/labqs-hom.sh
+++ xen/vms/labqs-hom.sh
@@ -2,11 +2,11 @@
 set -euo pipefail
 
 sudo xen-create-image \
-    --hostname='c1.labqs.ita.br' \
-    --memory=4gb \
+    --hostname='labqs.ita.br' \
+    --memory=8gb \
     --vcpus=2 \
     --lvm=ubuntu-vg  \
-    --size=20Gb \
+    --size=200Gb \
     --ip=172.31.100.1 \
     --netmask=172.31.0.0 \
     --broadcast=172.31.255.255 \

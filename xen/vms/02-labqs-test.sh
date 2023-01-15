af0e1034aa497592c499ca52326acd6544ea1a39
José Marcos Gomes <jose.marcos.gomes@gmail.com>
2023-01-15 12:41:52 -0300

    criar ambientes

---
+++ xen/vms/02-labqs-test.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+sudo xen-create-image \
+    --hostname='dev.labqs.ita.br' \
+    --memory=8gb \
+    --vcpus=2 \
+    --lvm=ubuntu-vg  \
+    --size=200Gb \
+    --ip=172.31.100.2 \
+    --broadcast=172.31.255.255 \
+    --netmask=255.255.0.0 \
+    --gateway=172.31.0.1 \
+    --nameserver=161.24.23.180 \
+    --randommac \
+    --bridge=xenbr0 \
+    --role=labqs \
+    --pygrub \
+    --dist=focal \
+    --password='c0r0n@' \
+    --verbose
